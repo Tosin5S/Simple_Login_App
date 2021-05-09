08bc44a89e66b39310b2c8e2d7a981e519f8be31
5s <olyguy.aq@gmail.com>
2021-05-09 08:08:07 +0100

    Modification of somethings

--- app/src/main/java/com/x5s/simple_login_app/DetailActivity.kt
+++ app/src/main/java/com/x5s/simple_login_app/DetailActivity.kt
@@ -2,10 +2,31 @@ package com.x5s.simple_login_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.x5s.simple_login_app.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    private var binding: ActivityDetailBinding? = null
+    private var logo = 0
+    private var name: String? = null
+    private var detail: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        logo = intent.getIntExtra(FactAdapter.LOGO_EXTRAs,0)
+        name = intent.getStringExtra(FactAdapter.NAME_EXTRAS)
+        detail = intent.getStringExtra(FactAdapter.FACT_EXTRAS)
+        setUpFactDetail()
+    }
+
+    private fun setUpFactDetail(){
+        binding?.detailTv?.text =detail
+        binding?.logoIv?.setImageResource(logo)
+        title = name
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        binding = null
     }
 }
--- app/src/main/java/com/x5s/simple_login_app/DummyData.kt
+++ app/src/main/java/com/x5s/simple_login_app/DummyData.kt
@@ -5,29 +5,29 @@ object DummyData {
         AgTechModel(
             "Data preserved in soil",
             R.drawable.carrot_in_soil,
-            "Detail later"
+            "For traditional farming models perhaps the primary determinant of supply capacity is simply the availability and suitability of land. However, any idea of future potential must be built on current data, with what data there is then mapped to tell the story of a region. This story is effectively written in the dirt, the soil."
         ),
         AgTechModel(
             "Innovative agriculture moving underground",
             R.drawable.hydroponic_farm,
-            "Detail later"
+            "Sharp is growing strawberries in Dubia, while Sony, Toshiba and Fujitsu are all utilising former clean-room facilities at semiconductor plants across Japan for lettuce. These no-wash , no-soil greens are cultivated by means of hydroponics and grown at more than twice the speed of normal field production, thanks to specialised LED lighting to optimise photosynthesis."
         ),
         AgTechModel(
             "Greens fed on rainbow waste",
             R.drawable.plants_under_led_lights,
-            "Detail later"
+            "At Bioaqua Farm at Blackford in Somerset - the largest integrated aquaponic farm in Europe - vegetables are grown and Rainbow Trout reared together in organic symbiosis, without chemicals or pesticides, but with the help of bees and worms."
         ),
 
         AgTechModel(
             "Using the sun to generate freshwater",
             R.drawable.solar_panels,
-            "Detail later"
+            "One innovation in agriculture, offered by Sundrop Farms, draws on one of the few renewable resources in even more abundant supply than seawater - sunlight. Sundrop Farms harvests solar power to generate energy for desalination to supply hydroponic greenhouses."
         ),
 
         AgTechModel(
             "Agriculture by aircraft",
             R.drawable.drone_over_farm,
-            "Detail later"
+            "The Munich-based Quantum-Systems entry was a transition aircraft combining capabilities of a multicopter and fixed-wing model - vertical take-off, plus fast forward flight like a normal plane. Quantum VRT design allows farmers to adopt precise fertilisation strategies via accurate flight-planning software with evaluation of crop conditions, so reducing reliance on fertilisers and boosting yields."
         )
 
     )
